b3f1769d0db1698a364a2431b4a32ba01c29a17c
Itai Shirav <itais@infinidat.com>
2015-08-12 12:58:47 +0300

    Use FilterableField in InfinidatFilter for more advanced control over filtering

--- src/infi/django_rest_utils/filters.py
+++ src/infi/django_rest_utils/filters.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.exceptions import ValidationError as DjangoValidationError
 from django.template.loader import render_to_string
+from django.db.models import Q
 
 from rest_framework import filters
 from rest_framework.exceptions import ValidationError
@@ -14,7 +15,50 @@ IGNORE = [
 ]
 
 
+class FilterableField(object):
+    '''
+    Describes a field that can be filtered on.
+    name - the field name to use in the API.
+    source - the field name in the model, in case it is different than the name.
+             it can also be a callable f(field, orm_operator, value) --> Q object
+    converter - an optional function to modify the given filter value before passing it to the queryset.
+    datatype - the type of values that are expected for this field.
+    '''
+
+    STRING   = 'string'
+    INTEGER  = 'integer'
+    FLOAT    = 'float'
+    BOOLEAN  = 'boolean'
+    DATETIME = 'datetime'
+
+    def __init__(self, name, source=None, converter=None, datatype=STRING):
+        self.name = name
+        self.source = source or name
+        self.converter = converter or (lambda value: value)
+        self.datatype = datatype
+
+    def __unicode__(self):
+        return self.name
+
+    def convert(self, value):
+        if isinstance(value, list):
+            return [self.converter(v) for v in value]
+        else:
+            return self.converter(value)
+
+    def build_q(self, orm_operator, value):
+        value = self.convert(value)
+        if hasattr(self.source, '__call__'):
+            return self.source(self, orm_operator, value)
+        return Q(**{self.source + '__' + orm_operator: value})
+
+
 class InfinidatFilter(filters.BaseFilterBackend):
+    '''
+    Implements a filter backend that uses Infinidat's API syntax.
+    The serializer in use must implement get_filterable_fields that
+    returns a list of FilterableField instances.
+    '''
 
     def get_filter_description(self, view, html):
         if not html:
@@ -26,23 +70,44 @@ class InfinidatFilter(filters.BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
         filterable_fields = self._get_filterable_fields(view)
-        for field in request.GET.keys():
-            if field in IGNORE:
+        for field_name in request.GET.keys():
+            if field_name in IGNORE:
                 continue
-            if field not in filterable_fields:
-                raise ValidationError("Unknown filter field: '%s' (choices are %s)" % (field, ', '.join(filterable_fields)))
-            if len(request.GET.getlist(field)) > 1:
-                raise ValidationError("Filter field '%s' specified more than once" % field)
-            queryset = self._apply_filter(queryset, field, request.GET[field])
+            field = None
+            for f in filterable_fields:
+                if field_name == f.name:
+                    field = f
+                    break
+            if not field:
+                names = [f.name for f in filterable_fields]
+                raise ValidationError("Unknown filter field: '%s' (choices are %s)" % (field_name, ', '.join(names)))
+            if len(request.GET.getlist(field_name)) > 1:
+                raise ValidationError("Filter field '%s' specified more than once" % field_name)
+            queryset = self._apply_filter(queryset, field, request.GET[field_name])
         return queryset
 
     def _get_filterable_fields(self, view):
         serializer = view.get_serializer()
+        if hasattr(serializer, 'get_filterable_fields'):
+            return serializer.get_filterable_fields()
+        # Autodetect filterable fields
         return [
-            field.source or field_name
+            FilterableField(field.source or field_name, datatype=self._get_field_type(field))
             for field_name, field in serializer.fields.items()
             if not getattr(field, 'write_only', False) and not field.source == '*'
         ]
+
+    def _get_field_type(self, serializer_field):
+        from rest_framework.fields import BooleanField, IntegerField, FloatField, DecimalField, DateTimeField
+        if isinstance(serializer_field, BooleanField):
+            return FilterableField.BOOLEAN
+        if isinstance(serializer_field, IntegerField):
+            return FilterableField.INTEGER
+        if isinstance(serializer_field, FloatField) or isinstance(serializer_field, DecimalField):
+            return FilterableField.FLOAT
+        if isinstance(serializer_field, DateTimeField):
+            return FilterableField.DATETIME
+        return FilterableField.STRING
 
     def _apply_filter(self, queryset, field, expr):
         # Get operator and value
@@ -54,43 +119,41 @@ class InfinidatFilter(filters.BaseFilterBackend):
         kwargs = {}
         revert = False # if True, apply the inverse logic
         if op == 'eq':
-            kwargs[field] = value
+            q = field.build_q('exact', value)
         elif op == 'lt':
-            kwargs[field + '__lt'] = value
+            q = field.build_q('lt', value)
         elif op == 'le':
-            kwargs[field + '__lte'] = value
+            q = field.build_q('lte', value)
         elif op == 'gt':
-            kwargs[field + '__gt'] = value
+            q = field.build_q('gt', value)
         elif op == 'ge':
-            kwargs[field + '__gte'] = value
+            q = field.build_q('gte', value)
         elif op == 'ne':
-            kwargs[field] = value
-            revert = True
+            q = ~field.build_q('exact', value)
+        elif op == 'like':
+            q = field.build_q('icontains', value)
         elif op == 'in':
             vals = _parse_array(value)
             if not vals:
-                raise ValidationError(field + ': IN filter expects at least one value')
-            kwargs[field + '__in'] = vals
+                raise ValidationError(field.name + ': IN filter expects at least one value')
+            q = field.build_q('in', vals)
         elif op == 'out':
             vals = _parse_array(value)
             if not vals:
-                raise ValidationError(field + ': OUT filter expects at least one value')
-            kwargs[field + '__in'] = vals
-            revert = True
-        elif op == 'like':
-            kwargs[field + '__icontains'] = value
+                raise ValidationError(field.name + ': OUT filter expects at least one value')
+            q = ~field.build_q('in', vals)
         elif op == 'between':
-            vals = _parse_array(value)
+            vals = [field.convert(val) for val in _parse_array(value)]
             if len(vals) != 2:
-                raise ValidationError(field + ': BETWEEN filter expects exactly 2 values')
-            kwargs[field + '__range'] = [min(vals), max(vals)]
+                raise ValidationError(field.name + ': BETWEEN filter expects exactly 2 values')
+            q = field.build_q('range', vals)
         else:
-            raise ValidationError(field + ': unknown operator "{}"'.format(op))
+            raise ValidationError(field.source + ': unknown operator "{}"'.format(op))
         # Apply the filter to the queryset
         try:
-            return queryset.exclude(**kwargs) if revert else queryset.filter(**kwargs)
+            return queryset.filter(q)
         except (ValueError, DjangoValidationError):
-            raise ValidationError(field + ': the given operator or value are inappropriate for this field')
+            raise ValidationError(field.name + ': the given operator or value are inappropriate for this field')
 
 
 def _parse_array(expr):
