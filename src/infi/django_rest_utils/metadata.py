Infinidat/infi.django_rest_utils

commit 681ee4017786506c546ae7dc52f3aaf0038590e8

Added heuristic to decide whether OPTIONS requests should detail foreign key links of an object as choices.

src/infi/django_rest_utils/metadata.py
@@ -1,5 +1,10 @@
 from rest_framework import metadata
 from django.utils.encoding import force_text
+from .utils import get_approximate_count_for_all_objects
+from rest_framework import exceptions, serializers
+from django.db import connections
+from django.conf import settings
+
 
 
 class SimpleMetadata(metadata.SimpleMetadata):
@@ -13,6 +18,24 @@ class SimpleMetadata(metadata.SimpleMetadata):
         actions['GET'] = self.get_serializer_info(view.get_serializer())
         return actions
 
+    def should_detail_choices(self, field, field_info):
+        if field_info.get('read_only'):
+            return False
+        if not hasattr(field, 'choices'):
+            return False
+
+        if isinstance(field, (serializers.RelatedField, serializers.ManyRelatedField)):
+            approx_number_of_objects = get_approximate_count_for_all_objects(
+                connections[field.queryset.db].cursor(),
+                field.queryset.model._meta.db_table)
+            if hasattr(settings, 'MAX_CHOICES_TO_DETAIL_IN_API_META'):
+                if approx_number_of_objects:
+                    return approx_number_of_objects < settings.MAX_CHOICES_TO_DETAIL_IN_API_META
+                else:
+                    return queryset.count() < settings.MAX_CHOICES_TO_DETAIL_IN_API_META
+            else:
+                return True
+
     def get_field_info(self, field):
         """
         Overrides SimpleMetadata.get_field_info since the above was tweaked to prevent it from displaying related field
@@ -22,8 +45,7 @@ class SimpleMetadata(metadata.SimpleMetadata):
 
         """
         field_info = super(SimpleMetadata, self).get_field_info(field)
-
-        if not field_info.get('read_only') and hasattr(field, 'choices'):
+        if self.should_detail_choices(field, field_info):
             field_info['choices'] = [
                 {
                     'value': choice_value,

src/infi/django_rest_utils/pagination.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from django.conf import settings
 from django.db import connections
+from .utils import get_approximate_count_for_all_objects
 
 
 class LargeQuerySetPage(Page):
@@ -28,21 +29,6 @@ class LargeQuerySetPaginator(Paginator):
         self.approximated_number_of_objects = False
         self.limited_number_of_objects = False
 
-    def _get_approximate_count_for_all_objects(self):
-        # We count tuples in the queryset's table name, as well as possible
-        # child partitions such as <table>_y2016m12
-        sql = '''
-            SELECT sum(n_live_tup) FROM pg_stat_user_tables 
-            WHERE relname = '{}' or relname like '{}\_y%%';
-        '''
-        table = self.object_list.query.model._meta.db_table
-        cursor = connections[self.object_list.db].cursor()
-        cursor.execute(sql.format(table, table.replace('_', '\\_')))
-        result = int(cursor.fetchone()[0])
-        if result:
-            self.approximated_number_of_objects = True
-            return result
-        return self.object_list.count()
 
     def _get_limited_count(self):
         # Postgres is not good at counting, so we're limiting the count
@@ -60,7 +46,13 @@ class LargeQuerySetPaginator(Paginator):
             else:
                 # https://wiki.postgresql.org/wiki/Slow_Counting
                 # specifically, we can give an approximate count for all the rows in the table
-                self._count = self._get_approximate_count_for_all_objects()
+                approximation = get_approximate_count_for_all_objects(
+                    connections[self.object_list.db].cursor(),
+                    self.object_list.query.model._meta.db_table)
+                if approximation:
+                    self._count = approximation
+                else:
+                    self._count = self.object_list.count()
         return self._count
 
     def validate_number(self, number):

src/infi/django_rest_utils/utils.py
@@ -1,0 +1,9 @@
+def get_approximate_count_for_all_objects(cursor, table):
+    # We count tuples in the queryset's table name, as well as possible
+    # child partitions such as <table>_y2016m12
+    sql = '''
+        SELECT sum(n_live_tup) FROM pg_stat_user_tables
+        WHERE relname = '{}' or relname like '{}\_y%%';
+    '''
+    cursor.execute(sql.format(table, table.replace('_', '\\_')))
+    return int(cursor.fetchone()[0])

commit b474d89761ba94e565d6a26e4441e2cab028fec3

Fixed bug introduced in 681ee4017786506c546ae7dc52f3aaf0038590e8

src/infi/django_rest_utils/metadata.py
@@ -21,7 +21,7 @@ class SimpleMetadata(metadata.SimpleMetadata):
     def should_detail_choices(self, field, field_info):
         if field_info.get('read_only'):
             return False
-        if isinstance(field, (serializers.RelatedField, serializers.ManyRelatedField)):
+        if isinstance(field, serializers.RelatedField):
             approx_number_of_objects = get_approximate_count_for_all_objects(
                 connections[field.queryset.db].cursor(),
                 field.queryset.model._meta.db_table)