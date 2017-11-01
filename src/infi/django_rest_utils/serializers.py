4d4735f2c7e9b27249ae51ac5db9de2ed4be4795
Oded Badt <obadt@infinidat.com>
2017-11-01 18:36:01 +0200

    Fixed bug accidentally added by me in previous commit

--- src/infi/django_rest_utils/serializers.py
+++ src/infi/django_rest_utils/serializers.py
@@ -3,7 +3,6 @@ from infi.django_rest_utils.filters import OrderingField, FilterableField
 
 
 class DefaultModelSerializer(serializers.ModelSerializer):
-    counter = 0
 
     def get_filterable_fields(self):
         model = self.Meta.model
@@ -12,8 +11,3 @@ class DefaultModelSerializer(serializers.ModelSerializer):
     def get_ordering_fields(self):
         model = self.Meta.model
         return model.get_ordering_fields() if hasattr(model, 'get_ordering_fields') else OrderingField.for_model(model)
-
-    def to_representation(self):
-        if self.counter > 10:
-            raise Exception('Error')
-        self.counter = self.counter + 1
