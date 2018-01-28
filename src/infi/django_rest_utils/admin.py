e9e53592f545f0bf69d32e1e84a7d517c629faa2
Itay Galea <igalea@infinidat.com>
2018-01-28 15:38:04 +0200

    SPTL-1624 APIToken admin UI should display user and token columns

--- src/infi/django_rest_utils/admin.py
+++ src/infi/django_rest_utils/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from models import APIToken
 
-admin.site.register(APIToken)
+
+
+
+class APITokenAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'token')
+    list_filter = ('user', )
+    search_fields = ('user__username', 'token')
+
+
+
+admin.site.register(APIToken, APITokenAdmin)
