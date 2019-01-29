0f2e782d2a11fa182027c771e11879417248038a
Roy Belio <rbelio@infinidat.com>
2019-01-29 15:53:36 +0200

    Documentation trivial typo fix

--- src/infi/django_rest_utils/decorators.py
+++ src/infi/django_rest_utils/decorators.py
@@ -5,7 +5,7 @@ from django.utils.decorators import available_attrs, wraps
 def ajax_login_required(function):
     """
     Decorator for views that checks that the user is logged in, resulting in a
-    403 Unautherized response if not.
+    403 Unauthorized response if not.
     """
     @wraps(function, assigned=available_attrs(function))
     def wrapped_function(request, *args, **kwargs):
