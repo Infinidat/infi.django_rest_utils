ac98ded2e9fabbaee48821ec3eff515137889ed3
Ofer Elboher <oelboher@infinidat.com>
2023-06-05 15:08:45 +0000

    INV-323 Added a REST-API request to email a user's REST-API-token instead of...

--- src/infi/django_rest_utils/models.py
+++ src/infi/django_rest_utils/models.py
@@ -1,7 +1,9 @@
 from builtins import object
-from django.db import models
+
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 from django.utils.crypto import get_random_string
 
 
@@ -29,3 +31,21 @@ class APIToken(models.Model):
 
     def __str__(self):
         return self.token
+
+
+class UserActivity(models.Model):
+
+    seconds_interval_between_successive_rest_api_token_emails = 24 * 60 * 60  # The user may request that the token will be sent to him/her no more than once a day.
+
+    user                                = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, db_index=False)
+    last_rest_api_token_email_sent_at   = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return 'User activity for %s' % (self.user)
+
+    def may_send_rest_api_token_email(self):
+        if self.last_rest_api_token_email_sent_at:
+            current_time = timezone.now()
+            time_passed_since_last_rest_api_token_email_sent = current_time - self.last_rest_api_token_email_sent_at
+            return time_passed_since_last_rest_api_token_email_sent.total_seconds() >= UserActivity.seconds_interval_between_successive_rest_api_token_emails
+        return True  # No REST API token email yet sent to this user, so such an email may be sent now.
